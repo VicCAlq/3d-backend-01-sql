70f76b3b0afafd2b5dc25285f32925817944da97
lucasruan12 <lucas.ruan@nave.org.br>
2026-05-28 11:07:49 -0300

    lucas ruan atividade

--- src/respostas.js
+++ src/respostas.js
@@ -3,21 +3,25 @@
   * para selecionar na tabela cliente todos os clientes 
   * que moram em Jaboatão e Olinda
   */
-export const r1 = ``
+export const r1 = `select cidade from clientes
+where cidade in 
+("Jaboatão dos Guararapes", "Olinda");`
 
 /* Exercício 2:
   * Escreva entre as áspas da variável "r2" o comando 
   * para ordenar a tabela de produtos a partir do preço 
   * de cada item, em ordem do mais caro par ao mais barato
   */
-export const r2 = ``
+export const r2 = `select * from produtos
+order by preco desc;`
 
 /* Exercício 3:
   * Escreva entre as áspas da variável "r3" o comando 
-  * para filtrar os pedidos entre os meses de Outubro (10)
-  * e Novembro (11)
+  * para filtrar os pedidos entre os meses de DEZEMBRO (12)
+  * e DEZEMBRO (18)
   */
-export const r3 = ``
+export const r3 = `select data_pedido from pedidos
+bettween '2025-12-12' and '2025-12-18';`
 
 /* Exercício 1:
   * Escreva entre as áspas da variável "r4" o comando 
