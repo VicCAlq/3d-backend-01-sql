cdb360a17c6ddcbd9cbe6b3c352af48ee1b57257
zivshw <ziv.tabatchnik@nave.org.br>
2026-05-28 11:07:45 -0300

    respondidas 1-3

--- src/respostas.js
+++ src/respostas.js
@@ -3,22 +3,22 @@
   * para selecionar na tabela cliente todos os clientes 
   * que moram em Jaboatão e Olinda
   */
-export const r1 = ``
+export const r1 = `select nome, cidade from clientes where cidade in ("Jaboatão dos Guararapes", "Olinda");`
 
 /* Exercício 2:
   * Escreva entre as áspas da variável "r2" o comando 
   * para ordenar a tabela de produtos a partir do preço 
   * de cada item, em ordem do mais caro par ao mais barato
   */
-export const r2 = ``
+export const r2 = `select nome, preco from produtos order by preco desc;`
 
 /* Exercício 3:
   * Escreva entre as áspas da variável "r3" o comando 
-  * para filtrar os pedidos entre os dias 12 e 18 de Dezembro
+  * para filtrar os pedidos entre os dias 12 e 18 de Dezembro de 2025
   */
-export const r3 = ``
+export const r3 = `select data_pedido from pedidos where data_pedido between '2025-12-12' and '2025-18-12';`
 
-/* Exercício 4:
+/* Exercício 1:
   * Escreva entre as áspas da variável "r4" o comando 
   * para juntar informações da tabelas de produtos com
   * a tabela de pedidos
