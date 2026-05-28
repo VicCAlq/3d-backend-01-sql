50efdab458cebe4e8bad84961830b77c163845d8
PedroHenrique2D <pedrohenrique.barbosa@nave.org.br>
2026-05-28 11:04:49 -0300

    entregaparcial

--- src/respostas.js
+++ src/respostas.js
@@ -3,21 +3,25 @@
   * para selecionar na tabela cliente todos os clientes 
   * que moram em Jaboatão e Olinda
   */
-export const r1 = ``
+export const r1 = `select * from clientes
+where cidade in ('Jaboatão', 'Olinda');`
 
 /* Exercício 2:
   * Escreva entre as áspas da variável "r2" o comando 
   * para ordenar a tabela de produtos a partir do preço 
   * de cada item, em ordem do mais caro par ao mais barato
   */
-export const r2 = ``
+export const r2 = `select * from produtos 
+order by preco asc;`
 
 /* Exercício 3:
   * Escreva entre as áspas da variável "r3" o comando 
   * para filtrar os pedidos entre os meses de Outubro (10)
   * e Novembro (11)
   */
-export const r3 = ``
+export const r3 = `select * from pedidos 
+where data_pedido
+between '2025-12-12' and '2025-12-18';`
 
 /* Exercício 1:
   * Escreva entre as áspas da variável "r4" o comando 
