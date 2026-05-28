8b2a020bf111931823b348f6ccc6393e446f248d
kevenlwr <keven.santana@nave.org.br>
2026-05-28 10:53:47 -0300

    metade da atv feita

--- src/respostas.js
+++ src/respostas.js
@@ -3,21 +3,27 @@
   * para selecionar na tabela cliente todos os clientes 
   * que moram em Jaboatão e Olinda
   */
-export const r1 = ``
+export const r1 = `SELECT nome , email, cidade  FROM clientes 
+WHERE cidade in 
+('Jaboatão dos Guararapes', 'Olinda');
+`
 
 /* Exercício 2:
   * Escreva entre as áspas da variável "r2" o comando 
   * para ordenar a tabela de produtos a partir do preço 
   * de cada item, em ordem do mais caro par ao mais barato
   */
-export const r2 = ``
+export const r2 = `SELECT nome,preco,vendedor FROM produtos
+ORDER BY preco asc`
 
 /* Exercício 3:
   * Escreva entre as áspas da variável "r3" o comando 
   * para filtrar os pedidos entre os meses de Outubro (10)
   * e Novembro (11)
   */
-export const r3 = ``
+export const r3 = `SELECT cliente_id, valor_total, status FROM pedidos
+WHERE data_pedido
+BETWEEN '2025-12-12' AND '2025-18-12';`
 
 /* Exercício 1:
   * Escreva entre as áspas da variável "r4" o comando 
