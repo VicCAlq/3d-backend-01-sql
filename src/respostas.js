0d35518cce5ee29e99d925efc006ff0cb826cd15
Kirishima226 <davi.rabelo@nave.org.br>
2026-05-28 11:03:25 -0300

    klo

--- src/respostas.js
+++ src/respostas.js
@@ -3,19 +3,22 @@
   * para selecionar na tabela cliente todos os clientes 
   * que moram em Jaboatão e Olinda
   */
-export const r1 = ``
+export const r1 = `sElEcT nome, cidade from clientes 
+where cidade in
+('Jaboatão dos Guararapes') ('Olinda');`
 
 /* Exercício 2:
   * Escreva entre as áspas da variável "r2" o comando 
   * para ordenar a tabela de produtos a partir do preço 
   * de cada item, em ordem do mais caro par ao mais barato
   */
-export const r2 = ``
+export const r2 = `select nome, preco, vendedor from produtos
+order by preco desc;`
 
 /* Exercício 3:
   * Escreva entre as áspas da variável "r3" o comando 
-  * para filtrar os pedidos entre os meses de Outubro (10)
-  * e Novembro (11)
+  * para filtrar os pedidos entre os meses de Dezembro (12)
+  * e Dezembro (18)
   */
 export const r3 = ``
 
