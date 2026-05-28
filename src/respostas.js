8eb50d29e5552352a7b649bf50dd1906592108e8
Bruna561 <bruna.nobrega@nave.org.br>
2026-05-28 11:51:44 -0300

    primeira atividade do trimestre

--- src/respostas.js
+++ src/respostas.js
@@ -3,26 +3,28 @@
   * para selecionar na tabela cliente todos os clientes 
   * que moram em Jaboatão e Olinda
   */
-export const r1 = ``
+export const r1 = `select nome, cidade from clientes where cidade in ("Jaboatão", "Olinda");`
 
 /* Exercício 2:
   * Escreva entre as áspas da variável "r2" o comando 
   * para ordenar a tabela de produtos a partir do preço 
   * de cada item, em ordem do mais caro par ao mais barato
   */
-export const r2 = ``
+export const r2 = `select nome, preco from produtos order by preco desc;`
 
 /* Exercício 3:
   * Escreva entre as áspas da variável "r3" o comando 
-  * para filtrar os pedidos entre os meses de Outubro (10)
-  * e Novembro (11)
+  * para filtrar os pedidos entre os dias 12 e 18 de Dezembro de 2025
   */
-export const r3 = ``
+export const r3 = `select data_pedido from pedidos
+between '2025-12-12' and '2025-12-18';`
 
 /* Exercício 1:
   * Escreva entre as áspas da variável "r4" o comando 
   * para juntar informações da tabelas de produtos com
   * a tabela de pedidos
   */
-export const r4 = ``
+export const r4 = `select`
+
+
 
