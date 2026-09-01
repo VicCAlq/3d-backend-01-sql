4faee2ba77f75bd0c353f85bafd4742bf178ff2b
loisa188 <mariaheloisa.silva@nave.org.br>
2026-09-01 11:06:21 -0300

    Update dados_clientes.sql

--- src/dados/dados_clientes.sql
+++ src/dados/dados_clientes.sql
@@ -1,6 +1,5 @@
-INSERT INTO clientes (nome, email, telefone, cidade) VALUES
-('Analice Almeida', 'analice.97@bol.com.br', '(81) 98765-4321', 'Jaboatão dos Guararapes'),
-('Bartolomeu Bastos', 'barbastos@email.com', '(81) 91234-5678', 'Recife'),
-('Zaya Zacarias', 'zaza_vet@email.com', '(81) 99876-5432', 'Recife'),
-('Jorge Jordão', 'jojo_nordestino@email.com', '(81) 99887-7665', 'Olinda'),
-('Leila Lélis', 'cabele.leila.lelis@email.com', '(81) 98642-1357', 'Camaragibe');
+Mia Dolan', 'mia.dolan@lalaland.com', '(81) 98765-4321', 'Jaboatão dos Guararapes'),
+('Elizabeth Bennet', 'elizabeth.bennet@pemberley.com', '(81) 91234-5678', 'Recife'),
+('Sophie Paterson', 'sophie.paterson@aftersun.com', '(81) 99876-5432', 'Recife'),
+('Agnes Shakespeare', 'agnes.shakespeare@hamnet.com', '(81) 99887-7665', 'Olinda'),
+('hamnet', 'hamnet.extractor@inception.com', '(81) 98642-1357', 'Camaragibe')
