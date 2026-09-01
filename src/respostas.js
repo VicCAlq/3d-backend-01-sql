5a1635cdeac6872d2c8ef20e81a5549a3afe8265
netod4rk <severino.neto@nave.org.br>
2026-09-01 09:24:12 -0300

    Atividade de Rec Severino

--- src/respostas.js
+++ src/respostas.js
@@ -3,26 +3,26 @@
   * para selecionar na tabela cliente todos os clientes 
   * que moram em Jaboatão e Olinda
   */
-export const r1 = ``
+export const r1 = `SELECT * FROM cliente WHERE cidade IN ('Jaboatão', 'Olinda');`
 
 /* Exercício 2:
   * Escreva entre as áspas da variável "r2" o comando 
   * para ordenar a tabela de produtos a partir do preço 
   * de cada item, em ordem do mais caro par ao mais barato
   */
-export const r2 = ``
+export const r2 = `SELECT * FROM produtos ORDER BY preco DESC;`
 
 /* Exercício 3:
   * Escreva entre as áspas da variável "r3" o comando 
   * para filtrar os pedidos entre os meses de Outubro (10)
   * e Novembro (11)
   */
-export const r3 = ``
+export const r3 = `SELECT * FROM pedidos WHERE MONTH(data) BETWEEN 10 AND 11;`
 
-/* Exercício 1:
+/* Exercício 4:
   * Escreva entre as áspas da variável "r4" o comando 
   * para juntar informações da tabelas de produtos com
   * a tabela de pedidos
   */
-export const r4 = ``
+export const r4 = `SELECT * FROM produtos INNER JOIN pedidos ON produtos.id = pedidos.produto_id;`
 
